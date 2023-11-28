377ad36ca88b863302cf50cec7b00d2e04519c0a
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2023-11-28 23:28:24 +0530

    Update app.py
    Changed some exception code

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
-from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS,cross_origin
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
-logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
+
+logging.basicConfig(filename="scrapper.log", level=logging.INFO)
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['GET'])
+@app.route("/", methods=['GET'])
 def homepage():
     return render_template("index.html")
 
-@app.route("/review" , methods = ['POST' , 'GET'])
+@app.route("/review", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         try:
-            searchString = request.form['content'].replace(" ","")
+            searchString = request.form['content'].replace(" ", "")
             flipkart_url = "https://www.flipkart.com/search?q=" + searchString
             uClient = uReq(flipkart_url)
             flipkartPage = uClient.read()
@@ -27,7 +28,7 @@ def index():
             box = bigboxes[0]
             productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
             prodRes = requests.get(productLink)
-            prodRes.encoding='utf-8'
+            prodRes.encoding = 'utf-8'
             prod_html = bs(prodRes.text, "html.parser")
             print(prod_html)
             commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
@@ -39,52 +40,40 @@ def index():
             reviews = []
             for commentbox in commentboxes:
                 try:
-                    #name.encode(encoding='utf-8')
                     name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
-
                 except:
                     logging.info("name")
 
                 try:
-                    #rating.encode(encoding='utf-8')
                     rating = commentbox.div.div.div.div.text
-
                 except:
                     rating = 'No Rating'
                     logging.info("rating")
 
                 try:
-                    #commentHead.encode(encoding='utf-8')
                     commentHead = commentbox.div.div.div.p.text
-
                 except:
                     commentHead = 'No Comment Heading'
                     logging.info(commentHead)
+
                 try:
                     comtag = commentbox.div.div.find_all('div', {'class': ''})
-                    #custComment.encode(encoding='utf-8')
                     custComment = comtag[0].div.text
                 except Exception as e:
                     logging.info(e)
 
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
-                
+
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
-            return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
-        # except Exception as e:
-        #     logging.info(e)
-        #     return 'something is wrong'
+            return render_template('result.html', reviews=reviews[0:(len(reviews) - 1)])
         except Exception as e:
-         logging.error(f"An error occurred: {e}")
-        return f"An error occurred: {e}"
-
-        return render_template('results.html')
+            logging.error(f"An error occurred: {e}")
+            return f"An error occurred: {e}"
 
     else:
         return render_template('index.html')
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
